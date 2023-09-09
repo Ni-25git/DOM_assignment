7e71c1af24420c4aeebb8e9b277c267eef47ff63
Nipun Shaharavat <nipunsherawat@gmail.com>
2023-09-09 13:06:51 +0530

    doc records

--- PT-WEB-05 C2_905871/index.js
+++ PT-WEB-05 C2_905871/index.js
@@ -1,62 +1,44 @@
 let myForm=document.querySelector("form");
-let inpName=document.getElementById("name");
-let docId=document.getElementById("docID");
-let department=document.getElementById("dept");
-let expp=document.getElementById("exp");
-let emailId=document.getElementById("email");
-let contNum=document.getElementById("mbl");
-let tbody=document.querySelector("tbody");
-
-AllTask=[];
 
 myForm.addEventListener("submit",function(e){
     e.preventDefault();
 
-    let  Data=
-    {
-        input:inpName.value ,
-        Id:docId.value ,
-        deptSelect:department.value ,
-        docExp:expp.value ,
-        email:emailId.value ,
-        contact:contNum.value 
+    let name=document.getElementById("name").value;
+    let docId=document.getElementById("docID").value;
+    let specialization=document.getElementById("dept").value;
+    let experience=document.getElementById("exp").value;
+    let email=document.getElementById("email").value;
+    let mobile=document.getElementById("mbl").value;
 
-    }
-
-    AllTask.push(Data)
-    console.log(AllTask)
-    tbody.innerHTML="";
-
-    AllTask.map((ele)=>{
-        let tr=document.createElement("tr");
-        let td1=document.createElement("td");
-        let td2=document.createElement("td");
-        let td3=document.createElement("td");
-        let td4=document.createElement("td");
-        let td5=document.createElement("td");
-        let td6=document.createElement("td");
-
-        td1.innerText=ele.input;
-        td2.innerText=ele.Id;
-        td3.innerText=ele.deptSelect;
-        td4.innerText=ele.docExp;
-        td5.innerText=ele.email;
-        td6.innerText=ele.contact;
-
-        tr.append(td1,td2,td3,td4,td5,td6);
-    })
     let role;
-    if (experience > 5) {
-        role = "Senior";
-    } else if (experience >= 2 && experience <= 5) {
-        role = "Junior";
-    } else {
-        role = "Trainee";
-    }
-     let roleCell = document.createElement("td");
-     roleCell.innerText = role;
-     tr.append(roleCell)
-     tbody.append(tr)
+            if (experience > 5) {
+                role = "Senior";
+            } else if (experience >= 2 && experience <= 5) {
+                role = "Junior";
+            } else {
+                role = "Trainee";
+            }
+
+            let tbody=document.querySelector("tbody");
+            let tr=document.createElement("tr");
+            let data=[name,docId,specialization,experience,email,mobile,role];
+
+            data.forEach((ele)=>{
+                let td=document.createElement("td");
+                td.innerText=ele;
+                tr.appendChild(td);
+            })
+
+            let deleteTd=document.createElement("td");
+            let deleteBtn=document.createElement("button");
+            deleteBtn.innerText="Delete";
+
+            deleteBtn.addEventListener("click",function(e){
+                e.preventDefault();
+                tbody.removeChild(tr);
+            })
+            deleteTd.appendChild(deleteBtn);
+            tr.appendChild(deleteTd)
+
+            tbody.appendChild(tr);
 })
-
-
